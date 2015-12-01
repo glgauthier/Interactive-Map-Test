93def7b5f3158fde84be0423588101b2e5edc432
Nick Panzarino <panzarino23@gmail.com>
2015-12-02 00:13:36 +0100

    shadow the hedgehog

--- js/core/loading.js
+++ js/core/loading.js
@@ -6,7 +6,7 @@ var loadingScreen = {
     , radius: 20 // The radius of the inner circle
     , scale: 1 // Scales overall size of the spinner
     , corners: 1 // Corner roundness (0..1)
-    , color: '#000' // #rgb or #rrggbb or array of colors
+    , color: '#ffffff' // #rgb or #rrggbb or array of colors
     , opacity: 0.25 // Opacity of the lines
     , rotate: 0 // The rotation offset
     , direction: 1 // 1: clockwise, -1: counterclockwise
@@ -17,7 +17,7 @@ var loadingScreen = {
     , className: 'spinner' // The CSS class to assign to the spinner
     , top: '50%' // Top position relative to parent
     , left: '50%' // Left position relative to parent
-    , shadow: false // Whether to render a shadow
+    , shadow: true // Whether to render a shadow
     , hwaccel: false // Whether to use hardware acceleration
     , position: 'absolute' // Element positioning
     },
